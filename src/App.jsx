b1f91493f7568ce3225eac239136aea746c475be
Henry <global-vugwu6369@gmail.com>
2024-12-05 09:46:49 +0100

    my figma app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Product from './Pages/Product';
+import Contact from './Pages/Contact';
+//rafce
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About/> } />
+        <Route path='/product' element={ <Product/> } />
+        <Route path='/contact' element={ <Contact/> } />
+      </Routes>
+    </div>
+  )
+};
+
+export default App;
---
+++ src/Pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className='pt-[10vh]'>
+      <h2> Product page </h2>
+    </div>
+  )
+}
+
+export default Product;
