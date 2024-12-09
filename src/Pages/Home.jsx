b407c8840a50ef99acce2fed85e947a1940e21b7
Henry <global-vugwu6369@gmail.com>
2024-12-09 10:50:28 +0100

    added gallery page, partners page and testimonial page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Hero from '../InnerPages/Hero';
 import Features from '../InnerPages/Features';
 import Contents from '../InnerPages/Contents';
+import Gallery from '../InnerPages/Gallery';
+import Partners from '../InnerPages/Partners';
+import Testimonials from '../InnerPages/Testimonials';
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Features/>
       <Contents/>
+      <Gallery/>
+      <Partners/>
+      <Testimonials/>
     </div>
   )
 }
